8119bd59f0f675a5a2f709d7669fef30db2d75c3
Milani <milani@Mac-Pro-de-Milani.local>
2021-05-08 14:05:49 -0300

    feat: create login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,38 @@
+import { Flex,  Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
 export default function Home() {
-    return <h1>Dashgo</h1>;
+  return (
+    <Flex
+      w='100vw'
+      h='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as="form"
+        flexDir="column"
+        width="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt={6}
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }
