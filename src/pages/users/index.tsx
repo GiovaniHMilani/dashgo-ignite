51a1ae6d6129b3aa76d998e8294de3bee48e60e3
Milani <milani@Mac-Pro-de-Milani.local>
2021-05-14 12:38:55 -0300

    feat: add component to pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
   return (
@@ -77,9 +78,59 @@ export default function UserList() {
                   </Button>
                 </Td>
               </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Giovani Milani</Text>
+                    <Text fontSize="sm" color="gray.300">giovanimilani11@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  05 de Abril, 2021
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Giovani Milani</Text>
+                    <Text fontSize="sm" color="gray.300">giovanimilani11@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  05 de Abril, 2021
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
             </Tbody>
           </Table>
-
+          <Pagination />
         </Box>
       </Flex>
     </Box>
