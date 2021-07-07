dcf0db4a61067da1c908ee74ca0fc8e3c1832c4d
Giovani Milani <giovani.milani@ze.delivery>
2021-07-07 16:03:12 -0300

    create navigation

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,27 @@
-import { Link, Icon, Text, LinkProps as ChakraLinkProps } from '@chakra-ui/react'
-import { ElementType } from 'react'
+import {
+  Link as ChakraLink,
+  Icon,
+  Text,
+  LinkProps as ChakraLinkProps,
+} from "@chakra-ui/react";
+import { ElementType } from "react";
+import Link from "next/link";
 
 interface NavLinkProps extends ChakraLinkProps {
   children: string;
   icon: ElementType;
+  href: string;
 }
 
-export function NavLink({ children, icon, ...rest }: NavLinkProps) {
-  return(
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
+export function NavLink({ children, icon, href, ...rest }: NavLinkProps) {
+  return (
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
-  )
+  );
 }
