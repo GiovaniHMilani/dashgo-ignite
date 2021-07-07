29819e873f0838c498942dc40f869de48064694b
Giovani Milani <giovani.milani@ze.delivery>
2021-07-07 15:25:46 -0300

    change spacing in create user page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -6,7 +6,7 @@ import {
   VStack,
   SimpleGrid,
   HStack,
-  Button
+  Button,
 } from "@chakra-ui/react";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
@@ -20,19 +20,25 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-          <Heading size="lg" fontWeight="normal">Criar Usuario</Heading>
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+          <Heading size="lg" fontWeight="normal">
+            Criar Usuario
+          </Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+          <VStack spacing={["6", "8"]}>
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha"
+              />
             </SimpleGrid>
           </VStack>
 
